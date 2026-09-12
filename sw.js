9f52485a42bdc313a4ca7f2fae664c7e536d0ff9
Roniandrade323 <syk7rrrvhg@privaterelay.appleid.com>
2026-09-12 10:10:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE='dzat-03-v1';const A=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='dzat-04-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
